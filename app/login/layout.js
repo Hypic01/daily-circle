b7c915d995327d493ec4660ed7619c68470b2594
hypic01 <doldorijw@gmail.com>
2024-02-22 14:50:10 -0800

    used form for login page

--- app/login/layout.js
+++ app/login/layout.js
@@ -10,9 +10,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <>
         <Navbar />
         <body className={inter.className}>{children}</body>
-    </html>
+    </>
   );
 }
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,25 +1,65 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login with:", email, password);
+  };
+
   return (
     <section className="bg-white h-screen flex justify-between items-center px-36 pt-12">
       <div className="flex-col justify-center items-center ">
         <h1 className="text-black font-bold text-[52px] mb-20">Log in</h1>
-        <input placeholder="example@email.com" className="bg-[#000000] w-[565px] h-[60px] rounded-lg p-6 flex items-center mb-5 font-semibold text-[#ffffff] opacity-20" />
-        <input placeholder="Password" className="bg-[#000000] w-[565px] h-[60px] rounded-lg p-6 flex items-center mb-5 font-semibold text-[#ffffff] opacity-20" />
-        <div className="bg-[#DE483A] w-[565px] h-[60px] cursor-pointer rounded-lg p-6 flex items-center mb-12 text-white justify-center font-semibold">
-          Log In
-        </div>
-        <p className="underline text-black mb-2 font-semibold cursor-pointer">Create Account</p>
-        <p className="underline text-black font-semibold cursor-pointer">Forgot Password?</p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              required
+              id="email"
+              name="email"
+              value={email}
+              placeholder="example@email.com"
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-[#000000] w-[565px] h-[60px] rounded-lg p-6 flex items-center mb-5 font-semibold text-[#ffffff] opacity-20"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              required
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-[#000000] w-[565px] h-[60px] rounded-lg p-6 flex items-center mb-5 font-semibold text-[#ffffff] opacity-20"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-[#DE483A] w-[565px] h-[60px] cursor-pointer rounded-lg p-6 flex items-center mb-12 text-white justify-center font-semibold"
+          >
+            Log In
+          </button>
+        </form>
+
+        <p className="underline text-black mb-2 font-semibold cursor-pointer">
+          Create Account
+        </p>
+        <p className="underline text-black font-semibold cursor-pointer">
+          Forgot Password?
+        </p>
       </div>
       <div className="flex justify-center items-center">
-        <Image 
-            src="/stairs.png"
-            height={250}
-            width={250}
-        />
+        <Image src="/stairs.png" alt="stairs" height={250} width={250} />
       </div>
     </section>
   );
