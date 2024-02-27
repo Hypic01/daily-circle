e31cc11d97f9e9aa60ce20de0797ac89aa1f8c24
hypic01 <doldorijw@gmail.com>
2024-02-27 15:07:42 -0800

    hid navbar for dashboard

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 const signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
