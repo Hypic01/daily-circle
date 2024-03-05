89b51495be48a33f44f7a17fd407f5fd6ccfb2d4
hypic01 <doldorijw@gmail.com>
2024-03-05 15:15:16 -0800

    Added submit button and sign out button

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
       <div className="flex gap-4 items-center">
         <div className="w-[40px] h-[40px] rounded-xl bg-[#DE483A] cursor-pointer" />
-        <h1 className="font-bold text-black">Joonwoo Park</h1>
+        <h1 className="font-bold text-black">Test</h1>
         <button className="bg-[#CDCDCD] opacity-80 w-[85px] h-[42px] rounded-xl text-black font-base font-bold">Sign Out</button>
     </div>
   )
