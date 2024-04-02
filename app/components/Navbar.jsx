e32607345f0273fd01343e2ed2d0828fc7e4c4d7
hypic01 <doldorijw@gmail.com>
2024-04-02 16:37:54 -0700

    fixing navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className="px-36 top-3 w-full absolute bg-white flex justify-between items-center">
       <div className="flex">
-        <a href="/">
+        <Link href="/">
           <div className=" text-slate-800 text-base font-extrabold font-['Inter'] uppercase cursor-pointer">
             Daily Circle
           </div>
-        </a>
+        </Link>
       </div>
 
       <div className="flex gap-8 items-center">
@@ -26,13 +27,13 @@ const Navbar = () => {
             Start for free
           </p>
         </div> */}
-        <a href='/login'>
+        <Link href='/login'>
           <div className="w-[117px] h-[39px] bg-red-500 rounded-[10px] flex justify-center items-center cursor-pointer">
             <p className="text-white text-sm font-semibold font-['Inter'] leading-7">
               Log In
             </p>
           </div>
-        </a>
+        </Link>
 
       </div>
     </div>
--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { signOut, getAuth } from 'firebase/auth';
 import { app } from '../firebase/config.js';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Profile = () => {
   const router = useRouter();
@@ -17,7 +18,7 @@ const Profile = () => {
       // delete token
       localStorage.removeItem('user token');
       // redirect to home
-      router.push('/');
+      // router.push('/');
 
     } catch (error) {
       console.error("Sign out Error", error);
@@ -29,12 +30,13 @@ const Profile = () => {
     <div className="flex gap-4 items-center">
       <div className="w-[40px] h-[40px] rounded-xl bg-[#DE483A] cursor-pointer" />
       <h1 className="font-bold text-black">My Account</h1>
-      <button
+      <Link
+        href="/"
         onClick={handleClick}
         className="bg-[#CDCDCD] opacity-80 w-[85px] h-[42px] rounded-xl text-black font-base font-bold"
       >
         Sign Out
-      </button>
+      </Link>
     </div>
   )
 }
